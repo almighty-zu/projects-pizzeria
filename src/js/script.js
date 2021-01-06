c0c273043f883362120b85be9bf70530a60ba654
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-01-06 20:49:09 +0100

    Finish the initAccordion algorythm

--- src/js/script.js
+++ src/js/script.js
@@ -88,20 +88,26 @@
       const thisProduct = this;
 
       /*find the cliclable trigger(the element that should react to clicking)*/
-
       const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-
-      console.log('clickableTrigger:',clickableTrigger);
+      console.log('clickableTrigger:', clickableTrigger);
 
       /*START: add event listener to clickable trigger on event click*/
       clickableTrigger.addEventListener('click', function(event) {
+
         /*prevent default action for event*/
         event.preventDefault();
+
         /*find active product(product that has active class)*/
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+        console.log('active product:', activeProduct);
 
         /*if there is active product and it's not thisProduct.element, remove class active from it*/
+        if(activeProduct !== thisProduct.element && activeProduct !== null){
+          activeProduct.classList.remove('active');
+        }
 
         /*toggle active class on thisProduct.element*/
+        thisProduct.element.classList.toggle('active');
       });
     }
   }
