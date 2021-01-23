d22fa72cb16893c7b07d634a512aea6a622b1f78
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-01-23 20:41:00 +0100

    Add changes to initData

--- src/js/script.js
+++ src/js/script.js
@@ -72,6 +72,11 @@
     cart: {
       defaultDeliveryFee: 20,
     },
+    db: {
+      url: '//localhost:3131',
+      product: 'product',
+      order: 'order',
+    },
     // CODE ADDED END
   };
   const templates = {
@@ -476,7 +481,22 @@
     },
     initData: function () {
       const thisApp = this;
-      thisApp.data = dataSource;
+      thisApp.data = {};
+
+      const url = settings.db.url + '/' + settings.db.product;
+
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+
+          /*save parsedResponse as thisApp.data.products*/
+
+          /*execute initMenu method*/
+        });
+
     },
     initCart: function () {
       const thisApp = this;
